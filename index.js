928101857648572714d7f7b51e35be7d8d83654f
MateoR-gif <mateorendon008@gmail.com>
2024-08-21 20:27:42 -0500

    Database Connected

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 const port = 3000
+const uri = "mongodb+srv://Thew:8IJfg3igy0NhsF89@events-cluster.ntshh.mongodb.net/?retryWrites=true&w=majority&appName=events-cluster";
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function run() {
+  try {
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Base de Datos MongoDB Conectada");
+  } finally {
+    await mongoose.disconnect();
+  }
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('¡Hola Mundo!')
